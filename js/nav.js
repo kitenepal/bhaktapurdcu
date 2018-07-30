7c969f5a74271369d57dbcbcfe820a148db9e5ff
NidanStha <nidanshrestha@gmail.com>
2018-07-30 15:04:41 +0545

    nav responsive

--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,7 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-62px";
+    document.getElementById("navbar").style.top = "-64px";
   }
   prevScrollpos = currentScrollPos;
 }
