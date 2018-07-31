81ff5a58301711a9c405c1320777323d79e6d114
NidanStha <nidanshrestha@gmail.com>
2018-07-31 15:14:48 +0545

    nav js, nav collapse

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,12 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
+var wid;//85
+screen.width<=467?wid='-85':wid='-64';
 if (prevScrollpos-5 > currentScrollPos) {
   document.getElementById("navbar").style.top = "0";
 } else if(prevScrollpos+5< currentScrollPos) {
-  document.getElementById("navbar").style.top = "-64px";
+  document.getElementById("navbar").style.top = wid+'px';
 }
   prevScrollpos = currentScrollPos;
 }
