291a2038b881cadde80648cc8655da338fe4bf98
NidanStha <nidanshrestha@gmail.com>
2018-07-31 15:01:50 +0545

    scroll slide js correction

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,10 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-64px";
-  }
+if (prevScrollpos-5 > currentScrollPos) {
+  document.getElementById("navbar").style.top = "0";
+} else if(prevScrollpos+5< currentScrollPos) {
+  document.getElementById("navbar").style.top = "-64px";
+}
   prevScrollpos = currentScrollPos;
-  var myan=document.getElementById("myan").innerHTML=prevScrollpos;
-  var man=document.getElementById('man').innerHTML=currentScrollPos;
 }
