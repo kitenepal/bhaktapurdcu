d9482985adba2b58406adb993578b20c7c5e69c0
NidanStha <nidanshrestha@gmail.com>
2018-08-20 18:56:42 +0545

    edted nav

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
 var wid;//85
-screen.width<=467?wid='-95':wid='-65';
+screen.width<=467?wid='-98':wid='-90';
 if (prevScrollpos-5 > currentScrollPos) {
   document.getElementById("navbar").style.top = "0";
 } else if(prevScrollpos+5< currentScrollPos) {
