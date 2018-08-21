0c0d3922ad5e6be288603c83db96299e3829566a
NidanStha <nidanshrestha@gmail.com>
2018-08-21 17:26:02 +0545

    add marquee

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
 var wid;//85
-screen.width<=467?wid='-98':wid='-90';
+screen.width<=467?wid='-79':wid='-89';
 if (prevScrollpos-5 > currentScrollPos) {
   document.getElementById("navbar").style.top = "0";
 } else if(prevScrollpos+5< currentScrollPos) {
